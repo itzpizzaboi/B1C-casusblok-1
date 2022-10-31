3341ec4ce9b537c02cc64c8bb6d4c1be3fc68734
itzpizzaboi <mees.raaijmakers1@gmail.com>
2022-10-31 14:53:04 +0100

    added the movement of items across the line

--- CC_Pingu/functions.py
+++ CC_Pingu/functions.py
@@ -2,25 +2,23 @@ from time import sleep
 
 def lineFollow(robot, colorGround):
     speed = -100
-    rotationSpeed = -1.2
-    percentageStraight = 40
+    rotationSpeed = -1
+    percentageStraight = 50
 
     rotation = colorGround.reflection() - percentageStraight
     robot.drive(speed, rotationSpeed * rotation)
 
 def robotRotate(gyro, robotBase, speed, rotationTarget):
-
+    newRotationTarget = gyro.angle()
     gyro.reset_angle(0)
-    
+
     if rotationTarget < 0:
         while gyro.angle() > rotationTarget:
            robotBase.drive(0, speed)
-           print(gyro.angle())
 
     elif rotationTarget > 0:
         while gyro.angle() < rotationTarget:
             robotBase.drive(0, -speed)
-            print(gyro.angle())
 
     robotBase.drive(0,0)
     print(gyro.angle())
--- CC_Pingu/main.py
+++ CC_Pingu/main.py
@@ -9,6 +9,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.nxtdevices import ColorSensor as ColorSensor1
 import functions
+from time import sleep
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -29,21 +30,45 @@ distanceSensor = UltrasonicSensor(Port.S4)
 ev3 = EV3Brick()
 
 # Initialize driveBase
-robotBase = DriveBase(leftMotor, rightMotor, 55.5, 104)
+robotBase = DriveBase(leftMotor, rightMotor, 55.5, 105)
 gyro.reset_angle(0)
 
 # Initialize main
 def main():
+    
+    holding = True
     while True:
+        print(colorFront.color())
         functions.lineFollow(robotBase, colorGround)
         
         if colorGround.color() == Color.GREEN:
             robotBase.straight(-110)
             functions.rotationByLine(colorGround, robotBase, 30, 1, Color)
+            robotBase.drive(0, 12)
+            sleep(0.9)
 
         if colorGround.color() == Color.BLACK:
             robotBase.straight(-110)
-            functions.rotationByLine(colorGround, robotBase, 30, 1, Color)
+            functions.rotationByLine(colorGround, robotBase, 30, -1, Color)
+            robotBase.drive(0, 12)
+            sleep(0.9)
+        
+        if colorGround.color() == Color.RED:
+            robotBase.drive(0,0)
+            robotBase.straight(-110)
+            functions.robotRotate(gyro, robotBase, 30, -90)
+            robotBase.straight(-100)
+
+            if holding == True:
+                holding = False
+                armMotor.run_time(-200, 5000, wait=True)
+            
+            elif holding == False:
+                holding = True
+                armMotor.run_time(200, 5000, wait=True)
+            
+            robotBase.straight(100)
+            functions.robotRotate(gyro, robotBase, 30, 90)
 
 # Run main2
 main()
